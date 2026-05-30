720cd1eba02f9ea43708750f21726f2795b810af
Cyrillaryea <2425402727@live.gctu.edu.gh>
2026-05-30 13:16:28 +0000

    Add files via upload
    Scripting Languages Assignment 2

---
+++ CEL.py
@@ -0,0 +1,30 @@
+# File: CEL.py
+# Description: Print out my initials in large stylized letters
+# Assignment 1
+
+    # Name: Cyril Emmanuel Laryea
+    # Index No: 2425402727
+    # Email: 2425402727@live.gctu.edu.gh
+    # Grader: Augustus Buckman
+
+    #On my honor, Cyril Emmanuel Laryea, this Programming assignment is my own work
+    #and I have not provided this code to any other student
+
+def main():
+            # This program prints my initials in stylized large letters
+
+            print()
+            print(" ...CEL")
+            print()
+
+            print("...CCCCCCCCCCCC........EEEEEEEEEEEE........LLLL.............")
+            print("...CCC.................EEE.................LLLL.............")
+            print("...CCC.................EEE.................LLLL.............")
+            print("...CCC.................EEE.................LLLL.............")
+            print("...CCC.................EEEEEEEEEEEE........LLLL.............")
+            print("...CCC.................EEE.................LLLL.............")
+            print("...CCC.................EEE.................LLLL.............")
+            print("...CCC.................EEE.................LLLL.............")
+            print("...CCC............**...EEE............**...LLLL...........**")
+            print("...CCCCCCCCCCCC...**...EEEEEEEEEEEE...**...LLLLLLLLLLLL...**")
+main()
